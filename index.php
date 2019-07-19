004aeaa1f838a98552fcd0ddc9cbd51060b0462d
Nayeem Ibrahim <nayeemibrahim46@gmail.com>
2019-07-20 02:15:58 +0600

    Here is code for endup lesson 4

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo 'hello world now';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        header {
+            padding: 2cm;
+            text-align: center;
+            background-color: gray;
+            color: white;
+
+        }
+    </style>
+</head>
+<? htmlspecialchars ?>
+<body>
+    <header>
+        <h1>
+            <?= "Hello, ". htmlspecialchars($_GET['name']); ?>
+        </h1>
+    </header>
+</body>
+
+</html>
