5ec29a825dc52c00e66d181fc01a877931ee86a9
Nayeem Ibrahim <nayeemibrahim46@gmail.com>
2019-08-03 15:24:17 +0600

    Separate PHP Logic end of lesson 5

--- index.php
+++ index.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        header {
-            padding: 2cm;
-            text-align: center;
-            background-color: gray;
-            color: white;
+$greetings = "Hello Nayeem";
 
-        }
-    </style>
-</head>
-<? htmlspecialchars ?>
-<body>
-    <header>
-        <h1>
-            <?= "Hello, ". htmlspecialchars($_GET['name']); ?>
-        </h1>
-    </header>
-</body>
 
-</html>
+require "index.view.php";
---
+++ index.view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        header {
+            padding: 2cm;
+            text-align: center;
+            background-color: gray;
+            color: white;
+
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>
+            <?= $greetings; ?>
+        </h1>
+        
+    </header>
+</body>
+
+</html>
